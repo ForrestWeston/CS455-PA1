9687374fe6b47cb7c1216def206d21ebc0c97f15
ForrestWeston <Forrest.Weston@gmail.com>
2015-10-11 16:45:08 -0700

    Added most of the init stuff

--- client.c
+++ client.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 
+#define BUFFSIZE 256
+
+void ShowError(char *e)
+{
+	perror(e);
+	printf("USAGE: run <IP Addr> <Port>\n");
+	exit(1);
+}
 
 int main(char* argv[], int arcg)
 {
@@ -15,4 +23,36 @@ int main(char* argv[], int arcg)
 	//1.lenght: 2-byte binary number in network byte order htons()
 	//2.string: non-null terminatedo
 	//appon recv result of password, print, close socket, exit
+
+	struct sockaddr_in server_addr;
+	struct hostent *server;
+	int sockfd, portnum, err;
+	char buff[BUFFSIZE];
+
+	if (argc != 3) {
+		ShowError("ERROR wrong number of commands\n");
+	}
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		ShowError("ERROR opening socket\n");
+	}
+
+	portnum = atoi(argv[2]);
+	server = gethostbyname(argv[1]);
+	if (server == NULL) {
+		printf("ERROR no such host\n");
+		exit(0);
+	}
+
+	memset(&server_addr, 0, sizeof(sockaddr_in));
+	server_addr.sin_family = AF_INET;
+	memcpy(server->h_addr, &server_addr.sin_addr.s_arrd, server->h_length);
+	server_addr.sin_port = htons(portnum);
+
+	// Attempt to connect to the server
+	err = connect(sockfd, &server, sizeof(server_addr));
+	if (err < 0) {
+		ShowError("ERROR on connect\n");
+	}
 }
--- server.c
+++ server.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include <string.h>
 
+#define BUFSIZE 256
+
+void ShowError(char* e)
+{
+	perror(e);
+	exit(1);
+}
 
 int main(char* argv[], int arcg)
 {
@@ -13,7 +20,40 @@ int main(char* argv[], int arcg)
 	//if fail close socket
 	//recv passwor verify with ID and Name,
 	//don't know what this means, does the password have to be '<nameid>' possibly?
-	//if correct respond 'Congratulations <name>; you've just revealed the password 
+	//if correct respond 'Congratulations <name>; you've just revealed the password
 	//for <ID number> to the world!'with 2-byte length
 	//if fail respond 'Password Incorrect' with the 2-byte lenght
+
+	int sockfd, cliSockfd, portNum, cliAddrLen, err;
+	char buff[BUFFSIZE];
+	struct sockaddr_in serv_addr, cli_addr;
+
+	if (argc != 2) {
+		ShowError("ERROR wrong number of commands\n");
+	}
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		ShowError("ERROR opening socket\n");
+	}
+
+	memset(&serv_addr, 0, sizeof(sockaddr));
+	portNum = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(portNum);
+	serv_addr.sin_arrd.s_arrd = INADDR_ANY;
+
+	err = bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+	if (err < 0) {
+		ShowError("ERROR could not bind to socket\n");
+	}
+
+	listen(sockfd, 5);
+	cliAddrLen = sizeof(cli_addr);
+	cliSockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &cliAddrLen);
+	if (cliSockfd < 0) {
+		ShowError("ERROR on accept");
+	}
+
+
 }
