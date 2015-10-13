fe4ce98247d440910d435dc4693a0b8333a6a01b
ForrestWeston <Forrest.Weston@gmail.com>
2015-10-12 18:07:23 -0700

    Complete, now to test

--- client.c
+++ client.c
@@ -1,58 +1,70 @@
 //CLIENT
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include "helpme.h"
 
-#define BUFFSIZE 256
-
-void ShowError(char *e)
+int main(int argc, char* argv[])
 {
-	perror(e);
-	printf("USAGE: run <IP Addr> <Port>\n");
-	exit(1);
-}
-
-int main(char* argv[], int arcg)
-{
-	//accept 2 command line args, IP address of server, port server is listening
-	//connect upon recieveing cmd inputs
-	//if 'Welcome\n is not recieved close connection, print error message, exit
-	//else promt user for ID and Name lol hausers code is wrong for this part
-	//if !success, show failure, exit. !do not sent to closed socket!
-	//prompt user for password, send 1.length 2.password
-	//1.lenght: 2-byte binary number in network byte order htons()
-	//2.string: non-null terminatedo
-	//appon recv result of password, print, close socket, exit
-
 	struct sockaddr_in server_addr;
 	struct hostent *server;
-	int sockfd, portnum, err;
-	char buff[BUFFSIZE];
+	int sockfd, err;
+	char Pswd[32];
+	char Name[36];
+	char ID[16];
+	char* correct;
+	char* welcome;
+	const char sentinel = '\n';
 
 	if (argc != 3) {
-		ShowError("ERROR wrong number of commands\n");
+		ErrorExit("ERROR wrong number of commands\n");
 	}
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0) {
-		ShowError("ERROR opening socket\n");
+		ErrorExit("ERROR opening socket\n");
 	}
 
-	portnum = atoi(argv[2]);
 	server = gethostbyname(argv[1]);
 	if (server == NULL) {
 		printf("ERROR no such host\n");
 		exit(0);
 	}
-
-	memset(&server_addr, 0, sizeof(sockaddr_in));
+	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	memcpy(server->h_addr, &server_addr.sin_addr.s_arrd, server->h_length);
-	server_addr.sin_port = htons(portnum);
+	server_addr.sin_addr.s_addr = inet_addr(argv[1]);
 
-	// Attempt to connect to the server
-	err = connect(sockfd, &server, sizeof(server_addr));
+	err = connect(sockfd, (struct  sockaddr *) &server_addr, sizeof(server_addr));
 	if (err < 0) {
-		ShowError("ERROR on connect\n");
+		ErrorExit("ERROR on connect");
 	}
+
+	welcome = recv_sentinel(sockfd, sentinel);
+	puts(welcome);
+	if (strcmp(welcome, "Welcome") !=0 ) {
+		ErrorExit("ERROR message recieved was not 'Welcome'\n");
+	}
+
+	printf("Enter ID: ");
+	scanf("%s", ID);
+	send_sentinel(sockfd, ID, sentinel);
+
+	printf("Enter Name: ");
+	scanf("%s", Name);
+	send_sentinel(sockfd, Name, sentinel);
+
+	correct = recv_sentinel(sockfd, sentinel);
+	printf("Name: %s, ID: %s, Correct?: %s\n", Name, ID, correct);
+	if (strcmp(correct, "Success") != 0) {
+		ErrorExit("ERROR Incorrect Name or ID\n");
+	}
+
+	printf("Enter Password: ");
+	scanf("%s", Pswd);
+	send_length(sockfd, Pswd);
+
+	correct = recv_length(sockfd);
+	printf("%s\n", correct);
+
+	close(sockfd);
+	return 0;
+
+
 }
--- server.c
+++ server.c
@@ -1,59 +1,101 @@
 //SERVER
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include "helpme.h"
 
-#define BUFSIZE 256
+struct client {
+	int ID;
+	char name[32];
+	char pswd[32];
+};
 
-void ShowError(char* e)
+int main(int argc, char* argv[])
 {
-	perror(e);
-	exit(1);
-}
+	int serverfd, port, err;
+	struct sockaddr_in server_addr;
+	struct client table[3];
 
-int main(char* argv[], int arcg)
-{
-	//accept 2 command line arge, port to listen on
-	//do not quite till Ctrl+C
-	//once the client connects send 'Welcome\n'
-	//recieve ID and Name, look up in table, send 'Success\n' or 'Failure\n'
-	//if fail close socket
-	//recv passwor verify with ID and Name,
-	//don't know what this means, does the password have to be '<nameid>' possibly?
-	//if correct respond 'Congratulations <name>; you've just revealed the password
-	//for <ID number> to the world!'with 2-byte length
-	//if fail respond 'Password Incorrect' with the 2-byte lenght
+	strncpy(table[0].name, "Adam", 4);
+	strncpy(table[0].pswd, "password", 8);
+	table[0].ID = 0;
 
-	int sockfd, cliSockfd, portNum, cliAddrLen, err;
-	char buff[BUFFSIZE];
-	struct sockaddr_in serv_addr, cli_addr;
+	strncpy(table[1].name, "James", 5);
+	strncpy(table[1].pswd, "password", 8);
+	table[1].ID = 1;
+
+	strncpy(table[2].name, "Forrest", 7);
+	strncpy(table[2].pswd, "password", 8);
+	table[2].ID = 2;
+
 
 	if (argc != 2) {
-		ShowError("ERROR wrong number of commands\n");
+		ErrorExit("ERROR wrong number of commands\n");
 	}
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		ShowError("ERROR opening socket\n");
+	//will pick TCP if SOCK_STREAM is passed, which it is...
+	serverfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (serverfd < 0) {
+		ErrorExit("ERROR opening socket\n");
 	}
 
-	memset(&serv_addr, 0, sizeof(sockaddr));
-	portNum = atoi(argv[1]);
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(portNum);
-	serv_addr.sin_arrd.s_arrd = INADDR_ANY;
+	//init struct to '0's
+	memset(&server_addr, 0, sizeof(server_addr));
+	port = atoi(argv[1]);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	err = bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+	//bind the servers port to a socket
+	err = bind(serverfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
 	if (err < 0) {
-		ShowError("ERROR could not bind to socket\n");
+		ErrorExit("ERROR could not bind to socket\n");
 	}
 
-	listen(sockfd, 5);
-	cliAddrLen = sizeof(cli_addr);
-	cliSockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &cliAddrLen);
-	if (cliSockfd < 0) {
-		ShowError("ERROR on accept");
+	err = listen(serverfd, 5);
+	if (err < 0) {
+		ErrorExit("Im a deaf mute server, har har har\n");
 	}
 
+	//like deadmau5 yo beep beep blurp blurp
+	while (1<2) {
 
+		int clientfd;
+		int clientID;
+		char* clientName;
+		char* clientPswd;
+		struct sockaddr clientAddr;
+		unsigned int clientLen = sizeof(clientAddr);
+
+		clientfd = accept(serverfd, (struct sockaddr*)&clientAddr, &clientLen);
+		if (clientfd < 0) {
+			ErrorExit("ERROR unable to accept client connection\n");
+		}
+
+		send_sentinel(clientfd, "Welcome", '\n');
+		clientID = atoi(recv_sentinel(clientfd, SENTINEL));
+		printf("ID: %d\n", clientID);
+
+		clientName = recv_sentinel(clientfd, '\n');
+		printf("Name: %s\n", clientName);
+
+		if (clientID < sizeof(table) &&
+			clientID >= 0 &&
+			strcmp(table[clientID].name, clientName) == 0) {
+			send_sentinel(clientfd, "Success", '\n');
+		} else {
+			send_sentinel(clientfd, "Failure", '\n');
+			close(clientfd);
+			continue;
+		}
+
+		clientPswd = recv_length(clientfd);
+		if (strcmp(table[clientID].pswd, clientPswd) == 0) {
+			char completeMsg[BUFFSIZE];
+			sprintf(completeMsg, "Congratulations %s: you've just reveaked the password for %d to the world in plain-text!", clientName, clientID);
+			send_length(clientfd, completeMsg);
+		} else {
+			send_length(clientfd, "Password incorrect");
+		}
+		close(clientfd);
+
+	}
 }
+
